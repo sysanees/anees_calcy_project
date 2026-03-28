9de5c5ce632bef139f258f0b9d7ec81078643de0
anees <sysanees@yahoo.com>
2026-03-28 10:22:49 +0000

    added a line in dev branch

--- calcy.py
+++ calcy.py
@@ -2,3 +2,4 @@ Nathahar Starting with the calcy program
 we are starting addion program
 we are ending the calcy project
 we are ending the Nathahar calcy project
+we are devloping a fearutre in dev brach
